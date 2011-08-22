c2774926441c4bde3e0e02fc47ee39f98b51c7c2
de.inferi.ex.inferis <de.inferi.ex.inferis@310d217d-7bf6-f99b-4201-cd3193807049>
2011-08-22 12:36:37 +0000

    fixed ws
    git-svn-id: https://aperte-reports.googlecode.com/svn/trunk@111 310d217d-7bf6-f99b-4201-cd3193807049

---
+++ aperte-reports-ws/ws-client/src/test/java/pl/net/bluesoft/rnd/apertereports/ws/client/ReportGeneratorClientTest.java
@@ -0,0 +1,39 @@
+package pl.net.bluesoft.rnd.apertereports.ws.client;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.net.bluesoft.rnd.apertereports.common.utils.ReportGeneratorUtils;
+import pl.net.bluesoft.rnd.apertereports.common.xml.ws.GenerateReportRequest;
+import pl.net.bluesoft.rnd.apertereports.common.xml.ws.GenerateReportResponse;
+import pl.net.bluesoft.rnd.apertereports.common.xml.ws.ReportData;
+
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("aperte-reports-ws-client.xml")
+public class ReportGeneratorClientTest {
+    @Autowired
+    private ReportGeneratorWebServiceClient client;
+
+    @Test
+    public void testClient() throws IOException {
+        GenerateReportRequest request = new GenerateReportRequest();
+        ReportData reportData = new ReportData();
+        reportData.setFormat("HTML");
+        reportData.setId("someid");
+        reportData.setName("helloworld");
+        reportData.setCharacterEncoding("utf-8");
+
+        byte[] source = IOUtils.toByteArray(ClassLoader.getSystemResourceAsStream("test_jasper.jrxml"));
+
+        reportData.setSource(ReportGeneratorUtils.wrapBytesInDataHandler(source, "text/html"));
+        request.setReportData(reportData);
+
+        GenerateReportResponse response = client.requestGenerateReport(request);
+        System.out.println(new String(ReportGeneratorUtils.unwrapDataHandler(response.getContent())));
+    }
+}
