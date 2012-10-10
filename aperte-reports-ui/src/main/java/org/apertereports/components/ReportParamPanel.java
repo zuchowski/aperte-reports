16d8a5af776747883cca1c326a779e075519b1ba
Tomasz Serafin <tserafin@bluesoft.net.pl>
2012-10-10 15:28:55 +0200

    code reformat before functional changes

--- aperte-reports-ui/src/main/java/org/apertereports/components/ReportParamPanel.java
+++ aperte-reports-ui/src/main/java/org/apertereports/components/ReportParamPanel.java
@@ -17,45 +17,43 @@ import com.vaadin.ui.VerticalLayout;
 @SuppressWarnings("serial")
 public class ReportParamPanel extends Panel {
 
-	private ReportParametersComponent reportParametersComponent;
-	private HorizontalLayout buttons ;
-	
-	
+    private ReportParametersComponent reportParametersComponent;
+    private HorizontalLayout buttons;
 
-	public ReportParamPanel(ReportTemplate reportTemplate, boolean showFormat) {
-		try {
-			ReportMaster rm = new ReportMaster(reportTemplate.getContent(), reportTemplate.getId().toString(),
-					new ReportTemplateProvider());
-			this.reportParametersComponent = new ReportParametersComponent(rm, showFormat);
-			VerticalLayout vl = new VerticalLayout();
-			vl.addComponent(reportParametersComponent);
-			buttons = ComponentFactory.createHLayout(vl);
-			addComponent(vl);
-		} catch (AperteReportsException e) {
-			throw new AperteReportsRuntimeException(e);
-		}
-	}
-	
-	/**
-	 * Empty panel
-	 */
-	public ReportParamPanel() {
-	}
+    public ReportParamPanel(ReportTemplate reportTemplate, boolean showFormat) {
+        try {
+            ReportMaster rm = new ReportMaster(reportTemplate.getContent(), reportTemplate.getId().toString(),
+                    new ReportTemplateProvider());
+            this.reportParametersComponent = new ReportParametersComponent(rm, showFormat);
+            VerticalLayout vl = new VerticalLayout();
+            vl.addComponent(reportParametersComponent);
+            buttons = ComponentFactory.createHLayout(vl);
+            addComponent(vl);
+        } catch (AperteReportsException e) {
+            throw new AperteReportsRuntimeException(e);
+        }
+    }
 
-	public void addButton(Button button){
-		buttons.addComponent(button);
-	}
-	
-	public void addButtons(HorizontalLayout buttons){
-		replaceComponent(this.buttons, buttons);
-		this.buttons = buttons;
-	}
+    /**
+     * Empty panel
+     */
+    public ReportParamPanel() {
+    }
 
-	public Map<String, String> collectParametersValues() {
-		return reportParametersComponent.collectParametersValues();
-	}
+    public void addButton(Button button) {
+        buttons.addComponent(button);
+    }
 
-	public String getOuptutFormat() {
-		return reportParametersComponent.getSelectedFormat();
-	}
+    public void addButtons(HorizontalLayout buttons) {
+        replaceComponent(this.buttons, buttons);
+        this.buttons = buttons;
+    }
+
+    public Map<String, String> collectParametersValues() {
+        return reportParametersComponent.collectParametersValues();
+    }
+
+    public String getOuptutFormat() {
+        return reportParametersComponent.getSelectedFormat();
+    }
 }
