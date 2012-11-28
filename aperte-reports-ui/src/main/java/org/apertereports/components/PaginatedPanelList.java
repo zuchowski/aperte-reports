352221bc198199fcae99a5e5ad085b01b1ee40cc
Tomasz Serafin <tserafin@bluesoft.net.pl>
2012-11-28 11:04:14 +0100

    code reformat before functional changes

--- aperte-reports-ui/src/main/java/org/apertereports/components/PaginatedPanelList.java
+++ aperte-reports-ui/src/main/java/org/apertereports/components/PaginatedPanelList.java
@@ -12,156 +12,156 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.BaseTheme;
 
 /**
- * 
+ *
  * @author Zbigniew Malinowski
- * 
- *         Component paginating GUI lists.
- * 
- * @param <O>
- *            datamodel backing class
- * @param <W>
- *            class used to display properties of backing class objects in GUI
+ *
+ * Component paginating GUI lists.
+ *
+ * @param <O> datamodel backing class
+ * @param <W> class used to display properties of backing class objects in GUI
  */
 @SuppressWarnings("serial")
 public abstract class PaginatedPanelList<O, W extends Panel> extends VerticalLayout {
 
-	private static final String PAGINATED_LIST_BUTTON_NEXT = "paginated-list.button.next";
-	private static final String PAGINATED_LIST_BUTTON_PREVIOUS = "paginated-list.button.previous";
-	private String filter;
-	private int pageSize;
-	private int listSize;
-	private int pageNumber;
+    private static final String PAGINATED_LIST_BUTTON_NEXT = "paginated-list.button.next";
+    private static final String PAGINATED_LIST_BUTTON_PREVIOUS = "paginated-list.button.previous";
+    private String filter;
+    private int pageSize;
+    private int listSize;
+    private int pageNumber;
 
-	public PaginatedPanelList(int pageSize) {
-		this.pageSize = pageSize;
-	}
+    public PaginatedPanelList(int pageSize) {
+        this.pageSize = pageSize;
+    }
 
-	/**
-	 * Fetches filtered list's fragment.
-	 * 
-	 * @return
-	 */
-	protected abstract Collection<O> fetch(String filter, int firstResult, int maxResults);
+    /**
+     * Fetches filtered list's fragment.
+     *
+     * @return
+     */
+    protected abstract Collection<O> fetch(String filter, int firstResult, int maxResults);
 
-	/**
-	 * Creates GUI component displaying backing object's properties.
-	 * 
-	 * @param object
-	 *            backing object
-	 * @return GUI component
-	 */
-	protected abstract W transform(O object);
+    /**
+     * Creates GUI component displaying backing object's properties.
+     *
+     * @param object backing object
+     * @return GUI component
+     */
+    protected abstract W transform(O object);
 
-	/**
-	 * Counts filtered list's size.
-	 * 
-	 * @return
-	 */
-	protected abstract int getListSize(String filter);
+    /**
+     * Counts filtered list's size.
+     *
+     * @return
+     */
+    protected abstract int getListSize(String filter);
 
-	public void filter(String filter) {
-		this.filter = filter;
-		pageNumber = 0;
-		listSize = getListSize(filter);
-		refresh();
-	}
+    public void filter(String filter) {
+        this.filter = filter;
+        pageNumber = 0;
+        listSize = getListSize(filter);
+        refresh();
+    }
 
-	private void refresh() {
+    private void refresh() {
 
-		Collection<O> list = fetch(filter, calculateFirstResult(), pageSize);
-		removeAllComponents();
-		for (O o : list)
-			addComponent(transform(o));
-		if (listSize > pageSize)
-			addFooter();
-	}
+        Collection<O> list = fetch(filter, calculateFirstResult(), pageSize);
+        removeAllComponents();
+        for (O o : list) {
+            addComponent(transform(o));
+        }
+        if (listSize > pageSize) {
+            addFooter();
+        }
+    }
 
-	private int calculateFirstResult() {
-		return pageNumber * pageSize;
-	}
+    private int calculateFirstResult() {
+        return pageNumber * pageSize;
+    }
 
-	private void addFooter() {
-		HorizontalLayout hl = ComponentFactory.createHLayout(this);
-		hl.setMargin(true, false, false, false);
-		if (hasPrevious()) {
-			ComponentFactory.createButton(PAGINATED_LIST_BUTTON_PREVIOUS, BaseTheme.BUTTON_LINK, hl,
-					new ClickListener() {
+    private void addFooter() {
+        HorizontalLayout hl = ComponentFactory.createHLayout(this);
+        hl.setMargin(true, false, false, false);
+        if (hasPrevious()) {
+            ComponentFactory.createButton(PAGINATED_LIST_BUTTON_PREVIOUS, BaseTheme.BUTTON_LINK, hl,
+                    new ClickListener() {
 
-						@Override
-						public void buttonClick(ClickEvent event) {
-							previousPage();
+                        @Override
+                        public void buttonClick(ClickEvent event) {
+                            previousPage();
+                        }
+                    });
+        } else {
+            ComponentFactory.createSimpleLabel(PAGINATED_LIST_BUTTON_PREVIOUS, "", hl);
+        }
+        for (int i = 0; i < countPages(); i++) {
+            final int pageIndex = i;
+            String caption = "" + (pageIndex + 1);
+            if (pageIndex == pageNumber) {
+                ComponentFactory.createSimpleLabel(caption, "", hl);
+            } else {
+                ComponentFactory.createButton(caption, BaseTheme.BUTTON_LINK, hl, new ClickListener() {
 
-						}
-					});
-		} else
-			ComponentFactory.createSimpleLabel(PAGINATED_LIST_BUTTON_PREVIOUS, "", hl);
-		for (int i = 0; i < countPages(); i++) {
-			final int pageIndex = i;
-			String caption = "" + (pageIndex + 1);
-			if (pageIndex == pageNumber)
-				ComponentFactory.createSimpleLabel(caption, "", hl);
-			else
-				ComponentFactory.createButton(caption, BaseTheme.BUTTON_LINK, hl, new ClickListener() {
+                    @Override
+                    public void buttonClick(ClickEvent event) {
+                        showPage(pageIndex);
+                    }
+                });
+            }
+        }
+        if (hasNext()) {
+            ComponentFactory.createButton(PAGINATED_LIST_BUTTON_NEXT, BaseTheme.BUTTON_LINK, hl, new ClickListener() {
 
-					@Override
-					public void buttonClick(ClickEvent event) {
-						showPage(pageIndex);
+                @Override
+                public void buttonClick(ClickEvent event) {
+                    nextPage();
+                }
+            });
+        } else {
+            ComponentFactory.createSimpleLabel(PAGINATED_LIST_BUTTON_NEXT, "", hl);
+        }
 
-					}
-				});
-		}
-		if (hasNext()) {
-			ComponentFactory.createButton(PAGINATED_LIST_BUTTON_NEXT, BaseTheme.BUTTON_LINK, hl, new ClickListener() {
+    }
 
-				@Override
-				public void buttonClick(ClickEvent event) {
-					nextPage();
+    private void showPage(int pageNumber) {
+        if (hasPage(pageNumber)) {
+            this.pageNumber = pageNumber;
+            refresh();
+        }
+    }
 
-				}
-			});
-		} else
-			ComponentFactory.createSimpleLabel(PAGINATED_LIST_BUTTON_NEXT, "", hl);
+    private void nextPage() {
+        if (hasNext()) {
+            pageNumber++;
+        }
+        refresh();
+    }
 
-	}
+    private void previousPage() {
+        if (hasPrevious()) {
+            pageNumber--;
+        }
+        refresh();
+    }
 
-	private void showPage(int pageNumber) {
-		if (hasPage(pageNumber)) {
-			this.pageNumber = pageNumber;
-			refresh();
-		}
-	}
+    private int countPages() {
 
-	private void nextPage() {
-		if (hasNext())
-			pageNumber++;
-		refresh();
-	}
+        int count = listSize / pageSize + 1;
+        if (listSize % pageSize == 0) {
+            count--;
+        }
+        return count;
+    }
 
-	private void previousPage() {
-		if (hasPrevious())
-			pageNumber--;
-		refresh();
+    private boolean hasPage(int pageNumber) {
+        return pageNumber >= 0 && pageNumber < countPages();
+    }
 
-	}
+    private boolean hasPrevious() {
+        return pageNumber > 0;
+    }
 
-	private int countPages() {
-
-		int count = listSize / pageSize + 1;
-		if (listSize % pageSize == 0)
-			count--;
-		return count;
-	}
-
-	private boolean hasPage(int pageNumber) {
-		return pageNumber >= 0 && pageNumber < countPages();
-	}
-
-	private boolean hasPrevious() {
-		return pageNumber > 0;
-	}
-
-	private boolean hasNext() {
-		return pageNumber < countPages() - 1;
-	}
-
+    private boolean hasNext() {
+        return pageNumber < countPages() - 1;
+    }
 }
--- aperte-reports-ui/src/main/java/org/apertereports/components/ReportOrderBrowserComponent.java
+++ aperte-reports-ui/src/main/java/org/apertereports/components/ReportOrderBrowserComponent.java
@@ -34,151 +34,148 @@ import com.vaadin.ui.themes.BaseTheme;
 @SuppressWarnings("serial")
 public class ReportOrderBrowserComponent extends Panel {
 
-	private static final int PAGE_SIZE = 10;
-	private static final String COMPONENT_STYLE = "borderless light";
-	private static final String BACKGROUND_BUTTON_PARAMS = "background.button.params";
-	private static final String BACKGROUND_BUTTON_PREVIEW = "background.button.preview";
-	private static final String CREATE_DATE = "createDate";
-	private static final String REPORTNAME_STYLE = "h4";
-	private static final String REPORTNAME = "reportname";
-	private static final String REPORT_STATUS = "reportStatus";
-	
-	private PaginatedPanelList<ReportOrder, ReportOrderPanel> list;
-	
-	public ReportOrderBrowserComponent() {
-		
-	}
-	
-	@Override
-	public void attach() {
-		super.attach();
-		init();
-	}
+    private static final int PAGE_SIZE = 10;
+    private static final String COMPONENT_STYLE = "borderless light";
+    private static final String BACKGROUND_BUTTON_PARAMS = "background.button.params";
+    private static final String BACKGROUND_BUTTON_PREVIEW = "background.button.preview";
+    private static final String CREATE_DATE = "createDate";
+    private static final String REPORTNAME_STYLE = "h4";
+    private static final String REPORTNAME = "reportname";
+    private static final String REPORT_STATUS = "reportStatus";
+    private PaginatedPanelList<ReportOrder, ReportOrderPanel> list;
 
-	private void init() {
+    public ReportOrderBrowserComponent() {
+    }
 
-		ComponentFactory.createSearchBox(new TextChangeListener() {
-			
-			@Override
-			public void textChange(TextChangeEvent event) {
-				list.filter(event.getText());
-			}
-		}, this);
-		
-		list = new PaginatedPanelList<ReportOrder, ReportOrderBrowserComponent.ReportOrderPanel>(PAGE_SIZE) {
-			
-			@Override
-			protected ReportOrderPanel transform(ReportOrder object) {
-				return new ReportOrderPanel(object);
-			}
-			
-			@Override
-			protected int getListSize(String filter) {
-				return ReportOrderDAO.countMatching(filter);
-			}
-			
-			@Override
-			protected Collection<ReportOrder> fetch(String filter, int firstResult, int maxResults) {
-				return ReportOrderDAO.fetch(filter, firstResult, maxResults);
-			}
-		};
+    @Override
+    public void attach() {
+        super.attach();
+        init();
+    }
 
-		list.setMargin(true, false, false, false);
-		addComponent(list);
-		list.filter(null);
-		setStyleName(COMPONENT_STYLE);
-	}
-	
-	private class ReportOrderPanel extends Panel {
-		
+    private void init() {
 
-		private ReportOrder order;
-		private ReportOrderParamsPanel params;
-		private boolean paramsVisible;
-		
-		public ReportOrderPanel(ReportOrder order) {
-			this.order = order;
-			
-		}
-		@Override
-		public void attach() {
-			super.attach();
-			init();
-		}
-		
-		private void init() {
-			
-			setStyleName(COMPONENT_STYLE);
-			((AbstractLayout) getContent()).setMargin(false, false, false, false);
-			BeanItem<ReportOrder> item = new BeanItem<ReportOrder>(order);
-			GridLayout grid = new GridLayout(6, 1);
-			grid.setWidth("100%");
-			grid.setSpacing(true);
-			grid.setColumnExpandRatio(1, 1);
-			addComponent(grid);
-			ComponentFactory.createIcon(item, REPORT_STATUS, grid);
-			ComponentFactory.createLabel(new BeanItem<ReportTemplate>(order.getReport()), REPORTNAME, REPORTNAME_STYLE, grid);
-			
-			ComponentFactory.createCalendarLabel(item, CREATE_DATE, "", grid);
-			Button previewButton = ComponentFactory.createButton(BACKGROUND_BUTTON_PREVIEW, BaseTheme.BUTTON_LINK, grid, new ClickListener() {
-				
-				@Override
-				public void buttonClick(ClickEvent event) {
-					showReport();
-					
-				}
-			});
-			ComponentFactory.createButton(BACKGROUND_BUTTON_PARAMS, BaseTheme.BUTTON_LINK, grid, new ClickListener() {
-				
-				@Override
-				public void buttonClick(ClickEvent event) {
-					toggleParams();
-					
-				}
-				
-			});
+        ComponentFactory.createSearchBox(new TextChangeListener() {
 
-			if(order.getReportStatus() != Status.SUCCEEDED)
-				previewButton.setEnabled(false);
-			
-			params = new ReportOrderParamsPanel(order.getParametersXml());
-		}
-		private void showReport() {
-			byte[] reportData;
-			try {
-				reportData = ReportGeneratorUtils.decodeContent(this.order.getReportResult());
-			} catch (UnsupportedEncodingException e) {
-				throw new AperteReportsRuntimeException(e);
-			}
-			FileStreamer.showFile(getApplication(), this.order.getReport().getReportname(), reportData,
-					this.order.getOutputFormat());
-			
-		}
-		private void toggleParams() {
-			paramsVisible = !paramsVisible;
-			if(paramsVisible)
-				addComponent(params);
-			else
-				removeComponent(params);
-		}
-	}
-	
-	private class ReportOrderParamsPanel extends Panel {
-		
-		public ReportOrderParamsPanel(String paramsXml) {
-			Map<String, String> params = XmlReportConfigLoader.getInstance().xmlAsMap(paramsXml);
-			List<String> sortedParamNames = new ArrayList<String>(params.keySet());
-			Collections.sort(sortedParamNames);
-			PropertysetItem item = new PropertysetItem();
-			for (String string : sortedParamNames) {
-				item.addItemProperty(string, new ObjectProperty<String>(params.get(string)));
-			}
-			Form form = new Form();
-			form.setItemDataSource(item);
-			form.setReadOnly(true);
-			addComponent(form);
-			form.getLayout().setMargin(false);
-			((AbstractLayout) getContent()).setMargin(false, true, false, true);
-		}
-	}
+            @Override
+            public void textChange(TextChangeEvent event) {
+                list.filter(event.getText());
+            }
+        }, this);
+
+        list = new PaginatedPanelList<ReportOrder, ReportOrderBrowserComponent.ReportOrderPanel>(PAGE_SIZE) {
+
+            @Override
+            protected ReportOrderPanel transform(ReportOrder object) {
+                return new ReportOrderPanel(object);
+            }
+
+            @Override
+            protected int getListSize(String filter) {
+                return ReportOrderDAO.countMatching(filter);
+            }
+
+            @Override
+            protected Collection<ReportOrder> fetch(String filter, int firstResult, int maxResults) {
+                return ReportOrderDAO.fetch(filter, firstResult, maxResults);
+            }
+        };
+
+        list.setMargin(true, false, false, false);
+        addComponent(list);
+        list.filter(null);
+        setStyleName(COMPONENT_STYLE);
+    }
+
+    private class ReportOrderPanel extends Panel {
+
+        private ReportOrder order;
+        private ReportOrderParamsPanel params;
+        private boolean paramsVisible;
+
+        public ReportOrderPanel(ReportOrder order) {
+            this.order = order;
+        }
+
+        @Override
+        public void attach() {
+            super.attach();
+            init();
+        }
+
+        private void init() {
+
+            setStyleName(COMPONENT_STYLE);
+            ((AbstractLayout) getContent()).setMargin(false, false, false, false);
+            BeanItem<ReportOrder> item = new BeanItem<ReportOrder>(order);
+            GridLayout grid = new GridLayout(6, 1);
+            grid.setWidth("100%");
+            grid.setSpacing(true);
+            grid.setColumnExpandRatio(1, 1);
+            addComponent(grid);
+            ComponentFactory.createIcon(item, REPORT_STATUS, grid);
+            ComponentFactory.createLabel(new BeanItem<ReportTemplate>(order.getReport()), REPORTNAME, REPORTNAME_STYLE, grid);
+
+            ComponentFactory.createCalendarLabel(item, CREATE_DATE, "", grid);
+            Button previewButton = ComponentFactory.createButton(BACKGROUND_BUTTON_PREVIEW, BaseTheme.BUTTON_LINK, grid, new ClickListener() {
+
+                @Override
+                public void buttonClick(ClickEvent event) {
+                    showReport();
+                }
+            });
+            ComponentFactory.createButton(BACKGROUND_BUTTON_PARAMS, BaseTheme.BUTTON_LINK, grid, new ClickListener() {
+
+                @Override
+                public void buttonClick(ClickEvent event) {
+                    toggleParams();
+                }
+            });
+
+            if (order.getReportStatus() != Status.SUCCEEDED) {
+                previewButton.setEnabled(false);
+            }
+
+            params = new ReportOrderParamsPanel(order.getParametersXml());
+        }
+
+        private void showReport() {
+            byte[] reportData;
+            try {
+                reportData = ReportGeneratorUtils.decodeContent(this.order.getReportResult());
+            } catch (UnsupportedEncodingException e) {
+                throw new AperteReportsRuntimeException(e);
+            }
+            FileStreamer.showFile(getApplication(), this.order.getReport().getReportname(), reportData,
+                    this.order.getOutputFormat());
+        }
+
+        private void toggleParams() {
+            paramsVisible = !paramsVisible;
+            if (paramsVisible) {
+                addComponent(params);
+            } else {
+                removeComponent(params);
+            }
+        }
+    }
+
+    private class ReportOrderParamsPanel extends Panel {
+
+        public ReportOrderParamsPanel(String paramsXml) {
+            Map<String, String> params = XmlReportConfigLoader.getInstance().xmlAsMap(paramsXml);
+            List<String> sortedParamNames = new ArrayList<String>(params.keySet());
+            Collections.sort(sortedParamNames);
+            PropertysetItem item = new PropertysetItem();
+            for (String string : sortedParamNames) {
+                item.addItemProperty(string, new ObjectProperty<String>(params.get(string)));
+            }
+            Form form = new Form();
+            form.setItemDataSource(item);
+            form.setReadOnly(true);
+            addComponent(form);
+            form.getLayout().setMargin(false);
+            ((AbstractLayout) getContent()).setMargin(false, true, false, true);
+        }
+    }
 }
