f4e4d8df08e534b008831ecb99a3eeb5b28f28cb
Tomasz Serafin <tserafin@bluesoft.net.pl>
2012-11-29 11:26:06 +0100

    code reformat before functional changes

--- backbone/src/main/java/org/apertereports/backbone/util/ReportOrderPusher.java
+++ backbone/src/main/java/org/apertereports/backbone/util/ReportOrderPusher.java
@@ -15,32 +15,34 @@ import org.hibernate.criterion.Restrictions;
  * Helper class for creating a report order and pushing it to JMS queue.
  */
 public class ReportOrderPusher {
+
     /**
-     * Pushes a message to JMS. The message contains a single integer value which is the report order ID.
+     * Pushes a message to JMS. The message contains a single integer value
+     * which is the report order ID.
      *
      * @param id Report order ID
      */
     public static void addToJMS(Long id) {
-    	AperteReportsJmsFacade.sendOrderToJms(id, ConfigurationConstants.JNDI_JMS_QUEUE_GENERATE_REPORT);
+        AperteReportsJmsFacade.sendOrderToJms(id, ConfigurationConstants.JNDI_JMS_QUEUE_GENERATE_REPORT);
     }
 
-	
-
     /**
-     * Builds a new report order with given parameters. Returns <code>null</code> when the paramters
-     * cannot be converted to an XML representation or another report order with equal configuration already
+     * Builds a new report order with given parameters. Returns
+     * <code>null</code> when the paramters cannot be converted to an XML
+     * representation or another report order with equal configuration already
      * exists in database.
      *
-     * @param report         A Jasper report template
-     * @param parameters     Report paramters
-     * @param format         Output format
-     * @param recipientEmail A recipient email address that should receive the report generation result
-     * @param username       Order creator login
-     * @param replyToQ       JMS queue name the result should be replied to
+     * @param report A Jasper report template
+     * @param parameters Report paramters
+     * @param format Output format
+     * @param recipientEmail A recipient email address that should receive the
+     * report generation result
+     * @param username Order creator login
+     * @param replyToQ JMS queue name the result should be replied to
      * @return A configured report order
      */
     public static ReportOrder buildNewOrder(final ReportTemplate report, Map<String, String> parameters, String format,
-                                            String recipientEmail, String username, String replyToQ) {
+            String recipientEmail, String username, String replyToQ) {
         final ReportOrder reportOrder = new ReportOrder();
         reportOrder.setParametersXml(XmlReportConfigLoader.getInstance().mapAsXml(parameters));
         reportOrder.setOutputFormat(format);
@@ -49,15 +51,10 @@ public class ReportOrderPusher {
         reportOrder.setReport(report);
         reportOrder.setReplyToQ(replyToQ);
         Boolean alreadyExists = new WHS<Boolean>() {
+
             @Override
             public Boolean lambda() {
-                return !sess.createCriteria(ReportOrder.class).add(Restrictions.eq("report", report))
-                        .add(Restrictions.eq("parametersXml", reportOrder.getParametersXml()))
-                        .add(Restrictions.eq("outputFormat", reportOrder.getOutputFormat()))
-                        .add(Restrictions.eq("username", reportOrder.getUsername()))
-                        .add(Restrictions.eq("recipientEmail", reportOrder.getRecipientEmail()))
-                        .add(Restrictions.eq("replyToQ", reportOrder.getReplyToQ()))
-                        .add(Restrictions.isNull("reportResult")).list().isEmpty();
+                return !sess.createCriteria(ReportOrder.class).add(Restrictions.eq("report", report)).add(Restrictions.eq("parametersXml", reportOrder.getParametersXml())).add(Restrictions.eq("outputFormat", reportOrder.getOutputFormat())).add(Restrictions.eq("username", reportOrder.getUsername())).add(Restrictions.eq("recipientEmail", reportOrder.getRecipientEmail())).add(Restrictions.eq("replyToQ", reportOrder.getReplyToQ())).add(Restrictions.isNull("reportResult")).list().isEmpty();
             }
         }.p();
         if (alreadyExists) {
@@ -67,5 +64,4 @@ public class ReportOrderPusher {
         reportOrder.setId(id);
         return reportOrder;
     }
-    
 }
