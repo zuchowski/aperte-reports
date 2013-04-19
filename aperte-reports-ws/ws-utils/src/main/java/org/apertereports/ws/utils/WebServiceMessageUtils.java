23070b2dbb65da771088aa7ebe9649ee5b4acc2d
Tomasz Serafin <tserafin@bluesoft.net.pl>
2013-04-19 23:26:32 +0200

    Repair of compile error for web service.

--- aperte-reports-ws/ws-utils/src/main/java/org/apertereports/ws/utils/WebServiceMessageUtils.java
+++ aperte-reports-ws/ws-utils/src/main/java/org/apertereports/ws/utils/WebServiceMessageUtils.java
@@ -1,16 +1,19 @@
 package org.apertereports.ws.utils;
 
-import org.springframework.ws.WebServiceMessage;
-import org.apertereports.common.ARConstants;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
+import org.apertereports.common.ARConstants;
+import org.springframework.ws.WebServiceMessage;
 
 public class WebServiceMessageUtils {
+
+    private static final SimpleDateFormat DATETIME_FORMAT = new SimpleDateFormat(ARConstants.DATETIME_PATTERN);
+
     public static void printStackTraceToLog(Logger logger, Exception e) {
         StringWriter sw = new StringWriter();
         e.printStackTrace(new PrintWriter(sw));
@@ -23,9 +26,8 @@ public class WebServiceMessageUtils {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         try {
             message.writeTo(baos);
-            logger.info("Logging message at: " + ARConstants.DATETIME_FORMAT.format(new Date()) + "\n" + new String(baos.toByteArray()));
-        }
-        catch (IOException e) {
+            logger.info("Logging message at: " + DATETIME_FORMAT.format(new Date()) + "\n" + new String(baos.toByteArray()));
+        } catch (IOException e) {
             printStackTraceToLog(logger, e);
         }
     }
